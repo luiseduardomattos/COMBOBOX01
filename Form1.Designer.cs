d0296469f86e77a231b147fd6c867da4a04057b7
luiseduardomattos <114542913+luiseduardomattos@users.noreply.github.com>
2023-03-03 22:18:07 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace COMBOBOX01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbEscolha = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMensagem = new System.Windows.Forms.Label();
+            this.EscolhaClique = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbEscolha
+            // 
+            this.cmbEscolha.FormattingEnabled = true;
+            this.cmbEscolha.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12"});
+            this.cmbEscolha.Location = new System.Drawing.Point(229, 23);
+            this.cmbEscolha.Name = "cmbEscolha";
+            this.cmbEscolha.Size = new System.Drawing.Size(121, 21);
+            this.cmbEscolha.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Escolha um numero de 1 a 12";
+            // 
+            // lblMensagem
+            // 
+            this.lblMensagem.AutoSize = true;
+            this.lblMensagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMensagem.Location = new System.Drawing.Point(45, 102);
+            this.lblMensagem.Name = "lblMensagem";
+            this.lblMensagem.Size = new System.Drawing.Size(0, 13);
+            this.lblMensagem.TabIndex = 2;
+            this.lblMensagem.Visible = false;
+            // 
+            // EscolhaClique
+            // 
+            this.EscolhaClique.Location = new System.Drawing.Point(417, 23);
+            this.EscolhaClique.Name = "EscolhaClique";
+            this.EscolhaClique.Size = new System.Drawing.Size(200, 23);
+            this.EscolhaClique.TabIndex = 3;
+            this.EscolhaClique.Text = "Escolha um numero e Clique Aqui";
+            this.EscolhaClique.UseVisualStyleBackColor = true;
+            this.EscolhaClique.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(504, 81);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 34);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "SAIR";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.EscolhaClique);
+            this.Controls.Add(this.lblMensagem);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbEscolha);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbEscolha;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblMensagem;
+        private System.Windows.Forms.Button EscolhaClique;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMBOBOX01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (Convert.ToInt32(cmbEscolha.SelectedItem))
+            {
+                    case 1:
+                    lblMensagem.Text = "O mês 1 é equivalente a Janeiro";
+                    break;
+
+                    case 2:
+                    lblMensagem.Text = "O mês 2 é equivalente a Fevereiro";
+                    break;
+
+                    case 3:
+                    lblMensagem.Text = "O mês 3 é equivalente a Março";
+                    break;
+
+                    case 4:
+                    lblMensagem.Text = "O mês 4 é equivalente a Abril";
+                    break;
+
+                    case 5:
+                    lblMensagem.Text = " O mês 5 é equivalente a Maio";
+                    break;
+
+                    case 6:
+                    lblMensagem.Text = "O mês 6 é equivalente a Junho";
+                    break;
+
+                    case 7:
+                    lblMensagem.Text = "O mês 7 é equivalente a julho";
+                    break;
+
+                    case 8:
+                    lblMensagem.Text = "O mês 8 é equivalente a Agosto";
+                    break;
+
+                    case 9:
+                    lblMensagem.Text = "O mês 9 é equivalente a Setembro";
+                    break;
+
+                    case 10:
+                    lblMensagem.Text = "O mês 10 é equivalente a Outubro";
+                    break;
+
+                    case 11:
+                    lblMensagem.Text = "O mês 11 é equivalente a Novembro";
+                    break;
+
+                    case 12:
+                    lblMensagem.Text = "O mês   " + cmbEscolha+ " é equivalente a Dezembro";
+                    break;
+
+                default:
+                    lblMensagem.Text = "Por favor escolha uma opção de números; Mês não encontrado";
+                    break;
+            }
+        }
+    }
+}
